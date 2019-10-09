e880b46daa333352333924bbb35db52ea3b5a8fb
JamesGaier <33331904+JamesGaier@users.noreply.github.com>
2019-10-09 15:26:49 -0700

    Add files via upload
    James' stuff

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+const router = express.Router();
+var mysql = require("mysql");
+var ejs = require("ejs");
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "Garrysmod1^3"
+});
+con.connect((err) => {
+	if(err) throw err;
+	console.log("Connected!");
+	con.query("use mydb;", (err, result) => {
+		if(err) throw err;
+	});
+	con.query("INSERT INTO tbl(id, name) VALUES(333,'bob');", (err, result) => {
+		if(err) throw err;
+	});
+	con.query("SELECT * FROM tbl;", (err, result) => {
+		if(err) throw err;
+		console.log(result);
+	});
+});
+
+
+
+router.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname+"/index.html"));
+});
+
+app.use(express.static(__dirname+"/style"));
+
+
+app.use("/", router);
+app.listen(process.env.port || 3000);
+console.log("Running at port 3000");
