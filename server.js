f9de5343c4bed94e09116774f39623a9549eaeb5
Unknown <james.gaier003@gmail.com>
2019-10-26 00:40:13 -0700

    fixing file structure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const multer  = require('multer')
-const upload = multer({ dest: 'public/images' })
+const multer  = require('multer');
+const upload = multer({ dest: 'public/images' });
+const upload1 = multer();
 const app = express();
 const path = require("path");
 const port = 3000;
@@ -8,14 +9,17 @@ const router = express.Router();
 var mysql = require("mysql");
 var ejs = require("ejs");
 var bodyParser = require("body-parser");
-var urlEncodedParser = bodyParser.urlencoded({extended: false});
 
-/*
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname+"/"));
+
+
 var con = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: ""
+	password: "123"
 });
+/*
 con.connect((err) => {
 	if(err) throw err;
 	console.log("Connected!");
@@ -30,31 +34,51 @@ con.connect((err) => {
 		console.log(result);
 	});
 });
-*/
+*/ 
 
 
+
+app.post("/", upload1.none(), (req, res, next) => {
+	console.log("pass");
+	res.redirect("/");
+});
+
+app.post('/dress',upload1.none(), function (req, res, next) {
+	con.connect((err) => {
+		if(err) throw err;
+		con.query("use mydb;", (err, result) => {
+			if(err) throw err;
+		});
+
+		con.query("SELECT * FROM users WHERE username='"+req.body.username+"'", (err, result) => {
+			if(err) throw err;
+			// this is where validation goes
+
+		});
+	});
+	res.redirect("/dress");
+});
 
 
 
 app.post('/dress', upload.single('chosenFile'), function (req, res, next) {
 	const file = req.file;
-	console.log(req.body);
 	res.redirect('/dress');
 
 });
 
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname+"/index.html"));
 	
 });
 
-router.get("/dress", (req, res) => {
+app.get("/dress", (req, res) => {
 	res.sendFile(path.join(__dirname+"/dress.html"));
 });
 
-app.use(express.static(__dirname+"/public"));
 
 
-app.use("/", router);
+
+
 app.listen(process.env.port || 3000);
 console.log("Running at port 3000");
