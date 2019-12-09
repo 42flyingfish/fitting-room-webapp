715f49c436df9a271e5df2149828ad96ef82f805
KarenC <krnchu@gmail.com>
2019-12-09 04:45:41 -0800

    Added in modeling dresses to new models with OpenCV warping and ML5 PoseNet. Added better model images too.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,52 +1,6 @@
 // base path to dress.
 //let currentDress = "images/dress1/";
 
-
-/*
-* Switches the dress model
-*/
-function model(){ 
-  var e = document.getElementById("hips_meas");
-  var opt = e.options[e.selectedIndex].value;
-
-  let chest = document.getElementById("chest_meas").selectedIndex;
-  let waist = document.getElementById("waist_meas").selectedIndex;
-  let hips = document.getElementById("hips_meas").selectedIndex;
-
-  
-  document.getElementById("dress-img").style.display = "block";
-  /*document.getElementById("model-img").style.display("block");*/
-
-  let bodAvg = parseInt((chest+waist+hips)/3, 10);
-
-  switch (bodAvg) {
-    case 0:
-      //var pic = currentDress + "dress-xs.png";
-      document.getElementById("rec-size").innerHTML = "Model Size: XS";
-      break;
-    case 1:
-      //var pic = currentDress + "dress-s.png";
-      document.getElementById("rec-size").innerHTML = "Model Size: S";
-      break;
-    case 2:
-      //var pic = currentDress + "dress-m.png";
-      document.getElementById("rec-size").innerHTML = "Model Size: M";
-      break;
-    case 3:
-      //var pic = currentDress + "dress-l.png";
-      document.getElementById("rec-size").innerHTML = "Model Size: L";
-      break;
-    case 4:
-      //var pic = currentDress + "dress-xl.png";
-      document.getElementById("rec-size").innerHTML = "Model Size: XL";
-      break;
-    case 5:
-      //var pic = currentDress + "dress-xxl.png";
-      document.getElementById("rec-size").innerHTML = "Model Size: XXL";
-      break;
-  }
-}
-
 /*
 * Sets model picture from specified file uploaded
 */
